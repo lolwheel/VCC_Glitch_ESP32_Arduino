7b0ac6a046cc70923bee98a8ffa99892cd88fbed
lolwheel <lolwheel@protonmail.com>
2023-01-28 14:11:12 -0800

    Parametrize pins.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <Arduino.h>
 
+#include <array>
+
 #include "SoftwareSerial.h"
 
-const uint32_t POWER_PIN_MASK = (1 << 25) | (1 << 26);
+// BEGIN CONFIG
+constexpr std::array POWER_PINS{25, 26, 27};
+constexpr uint32_t DUT_SERIAL_RX = 19;
+constexpr uint32_t DUT_SERIAL_TX = 18;
+// END CONFIG
 
-#define DUT_SERIAL_RX 19
-#define DUT_SERIAL_TX 18
+namespace {
+
+template <typename T, size_t SIZE>
+constexpr T getFlagsFromPins(const std::array<T, SIZE> &pins) {
+  T flags = 0;
+  for (size_t i = 0; i < SIZE; i++) {
+    flags |= (1 << pins[i]);
+  }
+  return flags;
+}
+constexpr uint32_t POWER_PIN_MASK = getFlagsFromPins(POWER_PINS);
+}  // namespace
 
 void inline IRAM_ATTR dutPowerOn() {
   REG_WRITE(GPIO_OUT_W1TS_REG, POWER_PIN_MASK);
@@ -15,12 +31,14 @@ void inline dutPowerOff() { REG_WRITE(GPIO_OUT_W1TC_REG, POWER_PIN_MASK); }
 
 void IRAM_ATTR asmGlitch() {
   asm volatile(
-      "movi a7, 0x6000000  \n\t"
+      "movi a7, %0  \n\t"
       "movi a8, 0x3ff4400c \n\t"
       "movi a9, 0x3ff44008 \n\t"
       "s32i a7, a8, 0      \n\t"
       "s32i a7, a9, 0      \n\t"
-      :::"a7", "a8", "a9");
+      : /* No outputs. */
+      : "n"(POWER_PIN_MASK)
+      : "a7", "a8", "a9");
 }
 
 // Glitch width is around 80 nanos, measured with a scope.
@@ -29,7 +47,7 @@ void IRAM_ATTR asmGlitch() {
 //   dutPowerOn();
 // }
 
-void inline IRAM_ATTR checksumBytes(uint8_t* bytes, int size) {
+void inline IRAM_ATTR checksumBytes(uint8_t *bytes, int size) {
   if (size == 1) {
     bytes[1] = ~bytes[0];
     return;
@@ -46,7 +64,7 @@ SoftwareSerial DutSerialRx;
 const uint8_t READ_MEM_BYTES[] = {0x11, 0xEE};
 void IRAM_ATTR powerCycleDut();
 
-bool IRAM_ATTR readAck(const char* message) {
+bool IRAM_ATTR readAck(const char *message) {
   uint8_t responseByte = 0;
   DutSerialRx.setTimeout(10);
   int readBytesNum = DutSerialRx.readBytes(&responseByte, 1);
@@ -72,8 +90,9 @@ void IRAM_ATTR powerCycleDut() {
 }
 
 void IRAM_ATTR setup() {
-  pinMode(25, OUTPUT);
-  pinMode(26, OUTPUT);
+  for (size_t i = 0; i < POWER_PINS.size(); i++) {
+    pinMode(POWER_PINS[i], OUTPUT);
+  }
   setCpuFrequencyMhz(240);
   Serial.begin(115200);
   DutSerialTx.begin(9600, SWSERIAL_8E1, -1, DUT_SERIAL_TX);
@@ -83,7 +102,7 @@ void IRAM_ATTR setup() {
   powerCycleDut();
 }
 
-uint32_t sendAndGlitchWithDelay(const uint8_t* bytes, size_t len,
+uint32_t sendAndGlitchWithDelay(const uint8_t *bytes, size_t len,
                                 uint32_t delay) {
   portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
   taskENTER_CRITICAL(&mux);
@@ -144,7 +163,6 @@ boolean readFirstpage(uint32_t delay) {
 }
 
 void IRAM_ATTR loop() {
-
   for (int j = 30; j < 80; j++) {
     if (readFirstpage(j)) {
       while (true)
